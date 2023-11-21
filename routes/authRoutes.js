45078cf1b3cb999a45d68f6154298a82612eee78
Marcos-Nobre <marcosnobre26@gmail.com>
2023-11-20 22:44:14 -0300

    teste

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,6 @@ const AuthController = require('../controllers/AuthController');
 //const authController = new AuthController();
 
 router.post('/login', AuthController.login);
-router.get('/testusers',AuthController.login);
+router.get('/testusers',AuthController.indextest);
 
 module.exports = router;

463233fbd36ec1631a14bfd7f1996bc72e964270
Marcos-Nobre <marcosnobre26@gmail.com>
2023-11-21 10:28:43 -0300

    teste conection

---
+++ test-connection.js
@@ -0,0 +1,24 @@
+const { Sequelize } = require('sequelize');
+const config = require('./caminho/para/config.json'); // Atualize o caminho conforme necessário
+
+const { username, password, database, host, dialect } = config.production;
+const sequelize = new Sequelize(database, username, password, {
+  host,
+  dialect,
+});
+
+// Teste de conexão
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão bem-sucedida!');
+  } catch (error) {
+    console.error('Erro ao conectar:', error);
+  } finally {
+    // Feche a conexão, se necessário
+    // await sequelize.close();
+  }
+}
+
+// Execute o teste de conexão
+testConnection();
